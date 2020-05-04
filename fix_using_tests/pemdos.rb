066683ce2e22886f0f49af818dbe228886f45ae6
James B Schoen <edensongjbs@gmail.com>
2020-05-04 18:52:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,13 +2,11 @@
 require 'pry'
 
 def snake_it_up(string)
-  #binding.pry
   if string[0] == "s"
      10.times do string="s" + string
    end
    string
   else
-    #binding.pry
     string
   end
 end
